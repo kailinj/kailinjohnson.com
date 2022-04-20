6ac359907b3f1a53355127dcea389cc4f881cffc
Kailin Johnson <kailinj@users.noreply.github.com>
2022-04-20 16:11:58 -0500

    Adjusts signature size & gap

--- src/app/signature/signature.component.ts
+++ src/app/signature/signature.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: "app-signature",
   templateUrl: "./signature.component.html",
+  styleUrls: ["./signature.component.scss"],
 })
 export class SignatureComponent implements OnInit {
   constructor() {}
