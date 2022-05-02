f9e4328f31cae01b06b3888fbc145e68f83258b8
Kailin Johnson <kailinj@users.noreply.github.com>
2022-05-02 18:06:11 -0500

    initial commit

--- src/app/resume/resume.module.ts
+++ src/app/resume/resume.module.ts
@@ -1,19 +1,18 @@
-import { CommonModule } from '@angular/common';
-import { NgModule } from '@angular/core';
-import { FlexLayoutModule } from '@angular/flex-layout';
-import { MatButtonModule } from '@angular/material/button';
-import { MatCardModule } from '@angular/material/card';
-import { MatIconModule } from '@angular/material/icon';
-import { MatListModule } from '@angular/material/list';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { RouterModule } from '@angular/router';
-import { NgScrollbarModule } from 'ngx-scrollbar';
+import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
+import { FlexLayoutModule } from "@angular/flex-layout";
+import { MatButtonModule } from "@angular/material/button";
+import { MatCardModule } from "@angular/material/card";
+import { MatIconModule } from "@angular/material/icon";
+import { MatListModule } from "@angular/material/list";
+import { MatSidenavModule } from "@angular/material/sidenav";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { RouterModule } from "@angular/router";
 
-import { PipesModule } from './../pipes/pipes.module';
-import { HistoryModule } from './../history/history.module';
-import { SkillsModule } from './../skills/skills.module';
-import { ResumeComponent } from './resume.component';
+import { PipesModule } from "./../pipes/pipes.module";
+import { HistoryModule } from "./../history/history.module";
+import { SkillsModule } from "./../skills/skills.module";
+import { ResumeComponent } from "./resume.component";
 
 @NgModule({
   declarations: [ResumeComponent],
@@ -21,7 +20,6 @@ import { ResumeComponent } from './resume.component';
     CommonModule,
     RouterModule,
     FlexLayoutModule,
-    NgScrollbarModule,
     MatButtonModule,
     MatCardModule,
     MatIconModule,
